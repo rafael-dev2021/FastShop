efff21457831dfa3e6ad1169cb7a4fa39acc8304
Rafael Silva <ainbot6@gmail.com>
2025-01-17 22:02:25 -0300

    feat: add fluent validations

---
+++ FluentValidations/Domain/Entities/Products/Fashion/F_Shoes/ObjectValues/MaterialObjectValueValidator.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities.Products.Fashion.F_Shoes.ObjectValues;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Products.Fashion.F_Shoes.ObjectValues;
+
+public class MaterialObjectValueValidator: AbstractValidator<MaterialObjectValue>
+{
+    public MaterialObjectValueValidator()
+    {
+        RuleFor(x => x.MaterialFromAbroad)
+            .NotEmpty().WithMessage("Materials from abroad cannot be empty.")
+            .MaximumLength(10).WithMessage("Materials from abroad must have a maximum length of 10 characters.");
+
+        RuleFor(x => x.MaterialInterior)
+            .NotEmpty().WithMessage("Interior materials cannot be empty.")
+            .MaximumLength(10).WithMessage("Interior materials must have a maximum length of 10 characters.");
+
+        RuleFor(x => x.MaterialSole)
+            .NotEmpty().WithMessage("Sole materials cannot be empty.")
+            .MaximumLength(10).WithMessage("Sole materials must have a maximum length of 10 characters.");
+    }
+    
+}
---
+++ FluentValidations/Domain/Entities/Products/Technology/T_Smartphones/ObjectValues/FeatureObjectValueValidator.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities.Products.Technology.T_Smartphones.ObjectValues;
+using FluentValidation;
+
+namespace FluentValidations.Domain.Entities.Products.Technology.T_Smartphones.ObjectValues;
+
+public class FeatureObjectValueValidator : AbstractValidator<FeatureObjectValue>
+{
+    public FeatureObjectValueValidator()
+    {
+        RuleFor(x => x.CellNetworkTechnology)
+            .NotEmpty().WithMessage("Cell network technology cannot be empty.")
+            .MaximumLength(30).WithMessage("Cell network technology must have a maximum length of 30 characters.");
+
+        RuleFor(x => x.VirtualAssistant)
+            .NotEmpty().WithMessage("Virtual assistant cannot be empty.")
+            .MaximumLength(50).WithMessage("Virtual assistant must have a maximum length of 50 characters.");
+
+        RuleFor(x => x.ManufacturerPartNumber)
+            .NotEmpty().WithMessage("Manufacturer part number cannot be empty.")
+            .MaximumLength(50).WithMessage("Manufacturer part number must have a maximum length of 50 characters.");
+    }
+}
