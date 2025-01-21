bc0f01c3adaed717426fc7b83ecf7200f0688acf
Rafael Silva <ainbot6@gmail.com>
2025-01-21 20:13:11 -0300

    feat: add new projects

---
+++ Infra-Ioc/InfrastructureModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra_Ioc;
+
+public static class InfrastructureModule
+{
+    public static void AddInfrastructureModule(this IServiceCollection service,
+        IConfiguration configuration)
+    {
+        service.AddDatabaseDependencyInjection(configuration);
+    } 
+}
