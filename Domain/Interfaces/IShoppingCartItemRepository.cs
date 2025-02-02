0cb07f599bf22145ae74fc4b72b05a013fbef8cb
Rafael Silva <ainbot6@gmail.com>
2025-02-02 17:43:27 -0300

    feat: add new interfaces

---
+++ Domain/Interfaces/IShoppingCartItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.Entities.Cart;
+
+namespace Domain.Interfaces;
+
+public interface IShoppingCartItemRepository
+{
+    Task<IEnumerable<ShoppingCartItem>> GetShoppingCartItemsAsync();
+    Task AddItemToCartAsync(Product product, Category category);
+    Task<int> RemoveItemToCartAsync(Product product, Category category);
+    Task<int> RemoveItemAsync(Product product);
+    Task<int> GetTotalCartItemsAsync();
+    Task<decimal> GetTotalAmountCartAsync();
+    Task ClearShoppingCartAsync();
+}
