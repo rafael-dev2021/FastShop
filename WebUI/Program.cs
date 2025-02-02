60a3350678cd6097178fb00f93745910f66fead0
Rafael Silva <ainbot6@gmail.com>
2025-02-02 17:48:58 -0300

    feat: add services to the container

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,10 +1,12 @@
 using Infra_Ioc;
+using Infra_Ioc.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddInfrastructureModule(builder.Configuration);
 builder.Services.AddControllersWithViews();
+builder.Services.AddEnUsCultureInfoDi();
 
 var app = builder.Build();
 
@@ -12,9 +14,9 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+await SeedData.SeedUsersRoles(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
